7b97a9fe7db7699b9e85038c56177a00edc1e1cc
Phase <ph4sesaber@gmail.com>
2015-01-17 00:51:47 -0800

    Project Start

---
+++ README.md
@@ -0,0 +1,3 @@
+The Eagle's Nest Hub
+=
+This is the main Hub plugin used at the spawn of the server, this does not handle anything about the Games, or Lobbies (since the Lobbies are pre-built into the Game).
---
+++ src/eagle/nest/hub/main/Hub.java
@@ -0,0 +1,17 @@
+package eagle.nest.hub.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Hub extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+	
+	}
+	
+}
