f855da3c292125260cf21d079c278cf418fb6133
Phase <ph4sesaber@gmail.com>
2015-01-19 06:20:05 -0800

    Updating to use IPUtils

--- src/eagle/nest/hub/listeners/HubListener.java
+++ src/eagle/nest/hub/listeners/HubListener.java
@@ -7,7 +7,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 import eagle.nest.hub.messages.MessageManager;
 import eagle.nest.hub.messages.Prefix;
-import eagle.nest.hub.util.skIP.SkIP;
+import eagle.nest.iputils.IPInfo;
 
 public class HubListener implements Listener {
 	
@@ -15,10 +15,11 @@ public class HubListener implements Listener {
 	@EventHandler
 	public void join(PlayerJoinEvent e){
 		Player p = e.getPlayer();
-		SkIP.SkIPData s = SkIP.getIPData(SkIP.getPlayerIP(p));
+		IPInfo.IPData s = IPInfo.getIPData(IPInfo.getPlayerIP(p));
 		MessageManager.broadcast(Prefix.Join, p.getName() + " joined from " +
-		s.getRegion() + ", " + 
-		s.getCountryName());
+				s.getCity() + ", " + 
+				s.getRegion() + ", " + 
+				s.getCountryName());
 	}
 	
 }
